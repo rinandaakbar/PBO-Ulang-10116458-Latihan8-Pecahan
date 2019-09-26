e7fb2a6ce93930eb15e9c76c96eba491722f49dc
nanda <nanda@nanda>
2019-09-26 21:59:06 +0700

    commit men

---
+++ src/pbo/ulang/pkg10116458/latihan8/pecahan/PBOUlang10116458Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116458.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Rinanda Al Qorrie Akbar
+ * NIM      : 10116458
+ */
+public class PBOUlang10116458Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
